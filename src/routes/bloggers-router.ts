861d0736cfee72413e066498c76796eeef41049d
yuraovdnk <yuraovdnk1999@ukr.net>
2022-05-19 18:42:44 +0300

    fix

--- src/routes/bloggers-router.ts
+++ src/routes/bloggers-router.ts
@@ -2,6 +2,7 @@ import {Router, Request, Response} from "express";
 import {bloggers, bloggersRepository} from "../repositories/bloggers-repository";
 import {bloggersValidate} from "../middlewares/bloggers-validator";
 import {checkAuth} from "../middlewares/auth";
+import {log} from "util";
 
 export const bloggersRouter = Router()
 
@@ -11,7 +12,7 @@ bloggersRouter.get('/', (req: Request, res: Response) => {
 
 bloggersRouter.get('/:id', (req: Request, res: Response) => {
     const id = +req.params.id
-    if(id){
+    if (id) {
         let foundBlogger = bloggersRepository.getBloggerById(id)
         if (foundBlogger) {
             res.status(200).send(foundBlogger)
@@ -21,36 +22,34 @@ bloggersRouter.get('/:id', (req: Request, res: Response) => {
     res.status(404).send()
 })
 
-bloggersRouter.post('/', checkAuth,bloggersValidate,(req: Request, res: Response) => {
-
+bloggersRouter.post('/', checkAuth, bloggersValidate, (req: Request, res: Response) => {
     const newBlogger = bloggersRepository.createBlogger(req.body)
 
     if (newBlogger) {
-        res.status(200).send(newBlogger)
+        res.status(201).send(newBlogger)
         return
     }
 
     res.send(404)
 })
 
-bloggersRouter.put('/:id',checkAuth,bloggersValidate,(req: Request, res: Response) => {
-    const id = req.params.id
-    if(id && typeof id === 'number'){
-        const updatedBlogger = bloggersRepository.updateBlogger(req.body, id)
-        if (updatedBlogger) {
-            res.send(204)
-            return
-        }
-        res.send(404)
+bloggersRouter.put('/:id', checkAuth, bloggersValidate, (req: Request, res: Response) => {
+    const id = +req.params.id
+    const updatedBlogger = bloggersRepository.updateBlogger(req.body, id)
+    if (updatedBlogger) {
+        res.send(204)
         return
     }
+    res.send(404)
+    return
+
 
     res.send(400)
 })
 
-bloggersRouter.delete('/:id',checkAuth, (req: Request, res: Response) => {
+bloggersRouter.delete('/:id', checkAuth, (req: Request, res: Response) => {
     const id = +req.params.id
-    if(id && typeof id === 'number'){
+    if (id && typeof id === 'number') {
         const deletedBlogger = bloggersRepository.deleteBlogger(id)
         if (deletedBlogger) {
             res.send(204)
